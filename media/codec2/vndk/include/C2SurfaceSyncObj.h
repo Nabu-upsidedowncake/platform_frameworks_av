littleGnAl/mirror_platform_frameworks_av

Codec2 vndk: clear pending lock by a dead client process
Bug: 312707275
Bug: 309898317
Bug: 304183768
Change-Id: I5c6c2bbdd4859220a3f41f83998d88d0a7b9ddcb

media/codec2/vndk/include/C2SurfaceSyncObj.h
@@ -122,6 +122,11 @@ struct C2SyncVariables {
      */
     void invalidate();
 
+    /**
+     * If a dead process holds the lock, clear the lock.
+     */
+    void clearLockIfNecessary();
+
     C2SyncVariables() {}
 
 private:
@@ -139,6 +144,11 @@ private:
      * wait for signal or broadcast.
      */
     int wait();
+
+    /**
+     * try lock for the specified duration.
+     */
+    bool tryLock(size_t ms);
 
     std::atomic<uint32_t> mLock;
     std::atomic<uint32_t> mCond;

Nabu-upsidedowncake/platform_frameworks_av

media/codec2/vndk/include/C2SurfaceSyncObj.h
@@ -122,6 +122,11 @@ struct C2SyncVariables {
      */
     void invalidate();
 
+    /**
+     * If a dead process holds the lock, clear the lock.
+     */
+    void clearLockIfNecessary();
+
     C2SyncVariables() {}
 
 private:
@@ -139,6 +144,11 @@ private:
      * wait for signal or broadcast.
      */
     int wait();
+
+    /**
+     * try lock for the specified duration.
+     */
+    bool tryLockFor(size_t ms);
 
     std::atomic<uint32_t> mLock;
     std::atomic<uint32_t> mCond;